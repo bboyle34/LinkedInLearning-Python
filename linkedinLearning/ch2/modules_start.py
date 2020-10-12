9804fa68d18b0062a959623545d6ad9897d07428
bboyle34 <bboyle34@gmail.com>
2020-10-12 12:41:20 -0400

    final files for chapter 2

---
+++ linkedinLearning/ch2/modules_start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+
+
+
+def main():
+    print("modules_start")
+
+    print("The square root of 81 is", math.sqrt(81))
+    print("Pi is:", math.pi)
+
+
+if __name__ == "__main__":
+    main()
