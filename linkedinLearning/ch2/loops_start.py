7c3e91923675e9ab21c72daa033765437629ddde
bboyle34 <bboyle34@gmail.com>
2020-10-12 12:40:04 -0400

    final files to chapter 2

--- linkedinLearning/ch2/loops_start.py
+++ linkedinLearning/ch2/loops_start.py
@@ -5,9 +5,27 @@ def main():
     print("loops_start")
 
     x = 0
-    while (x < 5):
-        print(x)
-        x = x + 1
+    #while (x < 5):
+        #print(x)
+        #x = x + 1
+    
+    #print()
+ 
+    #for x in range (5, 10):
+        #print(x)
+
+    print()
+    
+    days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i,d in enumerate(days):
+        print(i, d)
+
+    
+
+    #for x in range(5, 10):
+        #if (x==7):break
+        #if (x % 2 ==0): continue
+        #print(x) 
 
 if __name__ == "__main__":
     main()
