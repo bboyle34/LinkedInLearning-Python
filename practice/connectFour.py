5ab5e552d30e0c67b88f697eddac1c955ab35820
bboyle34 <bboyle34@gmail.com>
2020-11-06 12:34:50 -0500

    started the function that decides if someone won

--- practice/connectFour.py
+++ practice/connectFour.py
@@ -71,7 +71,8 @@ def move(board, player, mark):
                 if board[5 - i][move] == "0":
                     board[5 - i][move] = mark
                     #dumb = True
-                    #break
+                    smart = False
+                    break
                 if i == 5:
                     print("Column limit reached")
                     smart = True
@@ -81,7 +82,40 @@ def move(board, player, mark):
 #funciton to check if game has been won yet
 #check if 4 across, 4 vertical, 4 diagonal right, 4 diagonal left
 def gameCheck(board):
-    print("I have no clue if anyone is winning")
+    #have a counter that increments when there are two of the same tiles next to each other
+    #check each [i][j] for same tiles to the right, left, up, down, and horizontal
+    #right left [i][j+-1]
+    #up down [i+-1][j]
+    #horizontal [i-1][j-1] [i-1][j+1] [i+1][j+1] [i+1][j-1]
+    consec = 0
+    full = 0
+    while (full < 42 and consec != 4):
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if i == 0 and j == 0:
+                    #only check right, down, and rdown
+                elif i == 0 and j > 0 or j < 6
+                    #only check down, left, right, ldown, and rdown
+                elif i == 0 and j == 6:
+                    #only check right, down, and ldown
+                elif i > 0 and i < 5 and j == 0:
+                    #only check left, up, down, rup, and rdown
+                elif i > 0 and i < 5 and j > 0 and j < 6:
+                    #only check up, down, left, right, lup, rup, ldown, and rdown
+                elif i > 0 and i < 5 and j == 6:
+                    #only check left, up, down, lup, and ldown
+                elif i == 5 and j == 0:
+                    #only check right, down, rup
+                elif i == 5 and j > 0 and j < 6:
+                    #only check left, right, up, lup, and rup
+                elif i == 5 and j == 6:
+                    #only check left, up and lup
+                else:
+                    print("Idk how you got here")
+                    
+                full = full + 1
+                print(full)
+            
 
 
 if __name__ == "__main__":
